6d4bcf5ab03afb04c68cfc33a575fca41e9307c3
Ziad <zizoo1566@gmail.com>
2025-09-14 13:16:24 +0300

    Initial files and codes

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile("/views/index.html", {root: __dirname});
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
+
