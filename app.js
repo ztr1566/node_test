5bf94694f0f0da1cc1bf169dfd4371230d990530
Ziad <zizoo1566@gmail.com>
2025-09-16 17:08:26 +0300

    Code Update.....

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const add_user = require("./routes/add_user");
 const all_routes = require("./routes/all_routes");
